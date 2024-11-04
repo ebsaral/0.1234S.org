172f3252465dfd2c02a6948c86dc76346cc7f854
ebsaral <eminbugrasaral@me.com>
2024-11-04 11:19:53 +0300

    add legal disclaimer for all

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,6 +25,11 @@ export default function Layout({locale}: LayoutParams) {
     return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen w-full pb-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
+        <div className="row-start-3 flex gap-6 text-xs text-center font-bold font-[family-name:var(--font-geist-mono)] whitespace-pre-line">
+          {t("LegalWarning.title")}<br></br>
+          {t("LegalWarning.text")}
+        </div>
+
         <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <Image
               aria-hidden
