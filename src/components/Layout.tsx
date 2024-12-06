a9782d4a93795460ca770e083d4d9799c931ce56
ebsaral <eminbugrasaral@me.com>
2024-12-06 21:47:10 +0300

    minor update on warning text

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,7 @@ const Sections: SectionWithItemsType[] = [
 
 export default function Layout() {
     const t = useTranslations();
-    const lastUpdateDate = new Date("2024-12-04T14:21");
+    const lastUpdateDate = new Date("2024-12-04T21:46");
 
     return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen w-auto pb-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
