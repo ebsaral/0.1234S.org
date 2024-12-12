ca37e8f75b968e0a1a82c6eee5149b91a952c6ce
ebsaral <eminbugrasaral@me.com>
2024-12-12 07:21:59 +0300

    update translations

--- src/components/HazzetiEmin.tsx
+++ src/components/HazzetiEmin.tsx
@@ -6,7 +6,7 @@ import HomePageLink from "./HomePageLink";
 
 export default function HazzetiEmin() {
     const t = useTranslations();
-    const lastUpdateDate = new Date("2024-12-12T06:37");
+    const lastUpdateDate = new Date("2024-12-12T07:21");
     
     return <div className="w-auto mr-3 ml-3">
     <div className="flex-col flex gap-6 mt-2 flex-wrap items-center justify-center text-center w-auto m-auto mb-10">
@@ -28,6 +28,17 @@ export default function HazzetiEmin() {
     </div>
     
 
-    <div className="text-center mt-10 mb-5 text-sm"><LastUpdate date={lastUpdateDate} /></div>
+    <div className="text-center mt-10 mb-5 text-sm"><LastUpdate date={lastUpdateDate} isDivine={true} /></div>
+    <div className="row-start-4 flex flex-wrap items-center justify-center">
+      <iframe
+        width="100%"
+        height={166}
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/308359738&color=%23474136&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      />
+    </div>
+    
   </div>  
 }
