7d06f45fb32a5ebbf5f11b1bab6f1a9f39ab9966
ebsaral <eminbugrasaral@me.com>
2024-12-23 07:47:11 +0300

    release Edip Akbayram into TR (hell yeah)

--- src/data/sound/tr.ts
+++ src/data/sound/tr.ts
@@ -84,13 +84,29 @@ const paloma: Soundcloud = {
     }
 }
 
+const daglarDagladi: Soundcloud = {
+    src: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/287867484&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    info: {
+      artist: {
+          title: "dj tutan",
+          url: "https://soundcloud.com/dj-tutan"
+      },
+      song: {
+        title: "Edip Akbayram Daglar Dagladi Sound Sanayi Mix",
+        url: "https://soundcloud.com/dj-tutan/daglar-dagladi-sound-sanayi-mix"
+      }
+    }
+}
+
+
 const songs = [
     birYangininKulunu,
     kocaDunya,
     yesOrrayt,
     anlaGaripHalimden,
     isteHendekIsteDeve,
-    paloma
+    paloma,
+    daglarDagladi
 ]
 
 export default songs;
