5ee7c686c648e141fc81907d351eb198f2f5ee0c
Emin Saral <eminbugrasaral@me.com>
2025-03-09 13:54:56 +0300

    remove resume link

--- src/components/links/InfoLinks.tsx
+++ src/components/links/InfoLinks.tsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl"
-import { FaFilePdf } from "react-icons/fa6";
 import { MdOutlineEmail } from "react-icons/md";
 
 
@@ -7,14 +6,6 @@ export default function InfoLinks({className}: {className?: string}) {
     const t = useTranslations("Links")
     return (
         <div className={`flex gap-6 flex-wrap items-center justify-center ${className}`}>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href={t("cvLink")}
-          target="_blank"
-        >
-          <FaFilePdf />
-          {t("cvName")}
-        </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="mailto:eminbugrasaral@me.com"
