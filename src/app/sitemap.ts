7db7338743aa1f788629dab345b300d38d91a395
Emin Saral <eminbugrasaral@me.com>
2025-03-09 13:55:21 +0300

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -20,33 +20,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
           tr: 'https://ebsaral.vercel.app/tr/gallery',
         },
       },
-    },
-    {
-      url: 'https://ebsaral.vercel.app/files',
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          tr: 'https://ebsaral.vercel.app/tr/files',
-        },
-      },
-    },
-    {
-      url: 'https://ebsaral.vercel.app/hazzeti-emin',
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          tr: 'https://ebsaral.vercel.app/tr/hazzeti-emin',
-        },
-      },
-    },
-    {
-      url: 'https://ebsaral.vercel.app/asses',
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          tr: 'https://ebsaral.vercel.app/tr/asses',
-        },
-      },
     }
   ]
 }
