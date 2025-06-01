c80949e005461ae90a5f6771fcc0012db61239a6
Emin Saral <eminbugrasaral@me.com>
2025-06-01 17:21:17 +0300

    remove onCollapse from main

--- src/components/layout/HomeInfoSection.tsx
+++ src/components/layout/HomeInfoSection.tsx
@@ -1,13 +1,10 @@
 "use client"
 
-import { useState } from "react";
 import { useTranslations } from "next-intl";
-import { SparkleText, InfoItem } from "@/components";
+import { InfoItem } from "@/components";
 
 export default function HomeInfoSection() {
     const t = useTranslations("Pages.Home.Summary");
-    const [collapsed, setCollapsed] = useState(true);
-    const emoji = collapsed ? "😲" : "🤩";
     const infoSectionKeys = [
         "education",
         "work_experience",
@@ -18,31 +15,18 @@ export default function HomeInfoSection() {
         ]
     return (
         <>
-        <div id="summary" className="flex mb-6 text-center text-3xl font-bold font-[family-name:var(--font-geist-mono)] whitespace-pre-line select-none cursor-pointer" onClick={() => setCollapsed(!collapsed)}>
+        <div id="summary" className="flex mb-6 text-center text-3xl font-bold font-[family-name:var(--font-geist-mono)] whitespace-pre-line select-none cursor-pointer">
             <div className="flex flex-row gap-4">
-                <div>{emoji}</div>
-                <div className={collapsed ? "hover:underline underline-offset-8" : "gradient-purple"}><h2>{t("title")}</h2></div>
-                <div>{emoji}</div>
+                <div className="gradient-purple"><h2>{t("title")}</h2></div>
             </div>
         </div>
-        <div className={`flex-row space-y-2 gap-6 text-sm text-left sm:text-justify pl-5 pr-5 font-[family-name:var(--font-geist-mono)] whitespace-pre-line max-w-4xl ${collapsed? "overflow-hidden":""}`}>
-            <div className={`flex flex-col gap-6 ${collapsed? "h-64 sm:h-28":""}`}>
+        <div className={`flex-row space-y-2 gap-6 text-sm text-left sm:text-justify pl-5 pr-5 font-[family-name:var(--font-geist-mono)] whitespace-pre-line max-w-4xl`}>
+            <div className={`flex flex-col gap-6`}>
             {infoSectionKeys.map((infoKey, i)=>(
                 <InfoItem key={i} wide={false} title={t(`${infoKey}.title`)} text={t(`${infoKey}.text`)} />
             ))}
             </div>
         </div>
-        {collapsed && (
-            <div className="flex flex-col text-lg text-center justify-center cursor-pointer" onClick={() => setCollapsed(!collapsed)}>
-                <div 
-                    className="hover:underline underline-offset-4 select-none"
-                >
-                    <SparkleText />
-                </div>
-                <div className="text-4xl">🕺</div>
-            </div>
-            )
-        }
         </>
     )
 }
