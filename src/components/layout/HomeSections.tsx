a734f8365237e38880a12b233aa54f4002a87b7a
Emin Saral <eminbugrasaral@me.com>
2026-01-12 14:31:19 +0300

    Change gradient color of project titles

--- src/components/layout/HomeSections.tsx
+++ src/components/layout/HomeSections.tsx
@@ -13,7 +13,7 @@ export default function HomeSection() {
       content: projects,
       minCol: 1,
       maxCol: 3,
-      titleStyle: "gradient-blue text-lg"
+      titleStyle: "bg-gradient-to-r bg-clip-text text-transparent from-blue-400 via-pink-400 to-yellow-400 text-xl sm:text-xl"
     }
   ]
 
