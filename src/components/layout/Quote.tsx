ccdaf1133eff0d9f0fbba7e7d6e95787403cc9c8
Emin Saral <eminbugrasaral@me.com>
2026-01-12 14:30:42 +0300

    Remove width from quote box

--- src/components/layout/Quote.tsx
+++ src/components/layout/Quote.tsx
@@ -20,7 +20,7 @@ export default function Quote() {
   return (
     <div onClick={() => next()} className="relative flex align-middle items-center justify-center-80 p-6 border rounded-xl shadow-lg select-none cursor-pointer hover:bg-gray-800 hover:border-gray-800 min-h-60">
       <div className="absolute top-5 left-5 text-5xl">“</div>
-      <p className="w-60 text-center text-lg font-[family-name:var(--font-geist-mono)] whitespace-pre-line">
+      <p className="text-center text-lg font-[family-name:var(--font-geist-mono)] whitespace-pre-line">
         {content.items[index].title}
       </p>
       <p className="absolute left-4 bottom-7 text-xs text-right w-10">{index + 1}{" / "}{content.items.length}</p>
