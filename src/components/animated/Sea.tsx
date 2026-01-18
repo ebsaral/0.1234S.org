4ca61f4b8f2933c61b0e9c8eaf92a71abebf3199
Emin Saral <eminbugrasaral@me.com>
2026-01-18 21:06:24 +0300

    Add Sea to the bottom of all pages

---
+++ src/components/animated/Sea.tsx
@@ -0,0 +1,10 @@
+const Sea = ({ className }: { className?: string }) => {
+  return (
+    <div className={`sea ${className}`}>
+      <div className="wave back" />
+      <div className="wave" />
+    </div>
+  );
+};
+
+export default Sea;
